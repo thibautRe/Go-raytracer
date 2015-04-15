8c63bcedb774f841db09cc996e722615ca3477b3
Thibaut REMY <thibaut@domora.com>
2015-04-15 18:48:05 +0200

    Added json reader

--- main.go
+++ main.go
@@ -2,8 +2,26 @@ package main
 
 import (
     "fmt"
+    "encoding/json"
+    "io/ioutil"
 )
 
+type SceneDescriptor struct {
+    BackgroundColor string
+    Height int
+    Width int
+}
+
 func main() {
-    fmt.Println("Raytracer starting")
+    var scene SceneDescriptor
+    fileContent, err := ioutil.ReadFile("tests/test.json")
+    if err != nil {
+        fmt.Println(err)
+    }
+    err2 := json.Unmarshal(fileContent, &scene);
+    if err2 != nil {
+        fmt.Println(err2)
+    }
+
+    fmt.Println(scene, err);
 }
