212f24e98db488b5bac5f8824882c41821174cf8
Thibaut REMY <thibaut@domora.com>
2015-04-16 13:51:28 +0200

    Added GetUnit method

--- src/raymath/vector.go
+++ src/raymath/vector.go
@@ -10,8 +10,16 @@ func NewVector(p1 Point, p2 Point) Vector {
     return Vector{p1.X - p2.X, p1.Y - p2.Y, p1.Z - p2.Z}
 }
 
+////// METHODS //////
+
+
 func (v1 Vector) Minus(v2 Vector) Vector {
     return Vector{v1.X - v2.X, v1.Y - v2.Y, v1.Z - v2.Z}
+}
+
+func (v Vector) GetUnit() Vector {
+    a := v.Abs()
+    return Vector{v.X/a, v.Y/a, v.Z/a}
 }
 
 func (v Vector) Abs() float64 {
