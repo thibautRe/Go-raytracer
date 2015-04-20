924da8b235acd24704f8e0eb7a29cbae0f1749e7
Thibaut REMY <thibaut@domora.com>
2015-04-20 17:55:29 +0200

    Added time calculation

--- main.go
+++ main.go
@@ -7,11 +7,13 @@ import (
     "io/ioutil"
     "image/png"
     "./src/rayobject"
+    "time"
 )
 
 func main() {
+    now := time.Now()
     var scene rayobject.Scene
-    fileContent, err := ioutil.ReadFile("tests/multimat.json")
+    fileContent, err := ioutil.ReadFile("tests/plan.json")
     if err != nil {
         fmt.Println(err)
     }
@@ -21,7 +23,9 @@ func main() {
     }
 
     // Save the image
+    fmt.Println("Begin render...", scene.Samples, "samples,", scene.MaxRecursion, "max recursion")
     render := scene.Render()
+    fmt.Println("Render finished in", time.Since(now).Seconds(), "seconds")
     file, _ := os.Create("render/test.png")
     png.Encode(file, render)
 }
